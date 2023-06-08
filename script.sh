75d4430a4f0da3d641b2d21b5b3c95c8333da2f7
atsumi <atsumi.tatsuya@gmail.com>
2023-06-08 13:58:58 +0900

    fix script.sh

--- script.sh
+++ script.sh
@@ -8,17 +8,13 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
-curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
-echo '::endgroup::'
-
 echo '::group:: Installing graphql-schema-linter ...  https://github.com/cjoudrey/graphql-schema-linter'
-npm install graphql-schema-linter:"${INPUT_GRAPHQL_SCHEMA_LINTER_VERSION}" --global
-graphql-schema-linter --version
+npm install graphql-schema-linter:"${INPUT_GRAPHQL_SCHEMA_LINTER_VERSION}"
+"$(npm root)"/.bin/graphql-schema-linter --version
 echo '::endgroup::'
 
-
-graphql-schema-linter "${INPUT_GRAPHQL_SCHEMA_LINTER_FLAGS}" \
+# shellcheck disable=SC2086
+"$(npm root)"/.bin/graphql-schema-linter "${INPUT_GRAPHQL_SCHEMA_LINTER_FLAGS}" \
   | reviewdog -efm="%f:%l:%c: %m" \
       -name="graphql-schema-linter" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
